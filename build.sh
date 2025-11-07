6978ef0629d1ec68245d7f51b86fb1c065643e7f
Luna Nova <git@lunnova.dev>
2025-11-06 21:09:36 -0800

    theme: sass → modern css

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 [ -d public ] && rm -rf public
 
-swc compile --config-file .swcrc static/bsky-comments.jsx --out-dir ./
-sass --sourcemap=none theme/sass/theme.scss theme/static/theme.css
+swc compile --config-file .swcrc static/atproto-comments.jsx --out-dir ./
 cargo run --manifest-path ~/sync/dev/lun/rust-monorepo/site/Cargo.toml --release -- render . public/
 nix run nixpkgs#minify -- --html-keep-comments --html-keep-whitespace --html-keep-end-tags --html-keep-document-tags \
   --match="*.html" --match="*.css" --match="*.js" \
